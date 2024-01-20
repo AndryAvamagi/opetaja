e5d74b8cb50209a5beca2ee4b518206af6eaede5
AndryAvamagi <avamagi.andry@gmail.com>
2024-01-20 16:50:52 +0200

    setup and cleanup

---
+++ app/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyArBh0CLTP4LQHNeeTkYZrHgEC8rj0tUuc",
+  authDomain: "opetajapaneel.firebaseapp.com",
+  projectId: "opetajapaneel",
+  storageBucket: "opetajapaneel.appspot.com",
+  messagingSenderId: "952874758659",
+  appId: "1:952874758659:web:b654c627621fe6aaa6d436",
+  measurementId: "G-5CNYTKMZFY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
