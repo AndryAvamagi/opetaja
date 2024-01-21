385a523bcd026565c6fea3f09390c65ad5f00254
AndryAvamagi <avamagi.andry@gmail.com>
2024-01-21 17:28:29 +0200

    signin page main page, signup pole vaja

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,73 @@
-import Link from 'next/link';
+'use client'
+import signIn from "./firebase/auth/signIn";
+import { useRouter } from 'next/navigation';
+import { useState } from "react";
+// pmst copy paste sign upist, sign upi asemel sign in
+function Page(): JSX.Element {
+  const [ email, setEmail ] = useState( '' );
+  const [ password, setPassword ] = useState( '' );
+  const router = useRouter();
 
-export default function Home() {
+  const handleForm = async ( event: { preventDefault: () => void } ) => {
+    event.preventDefault();
+
+    const { result, error } = await signIn( email, password );
+
+    if ( error ) {
+      console.log( error );
+      return;
+    }
+
+    console.log( result );
+
+    router.push( "/admin" );
+  }
 
   return (
-    <main className="">
-      <div className="">
-          <Link href="/signin">
-            <span className="link">Login</span>
-          </Link>
-        </div>
-
-        <div className="">
-          <Link href="/signup">
-            <span className="link">Register</span>
-          </Link>
-        </div>
-    </main>
-  )
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="w-full max-w-xs">
+        <form onSubmit={handleForm} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <h1 className="text-3xl font-bold mb-6 text-black">Sign In</h1>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+              Email
+            </label>
+            <input
+              onChange={( e ) => setEmail( e.target.value )}
+              required
+              type="email"
+              name="email"
+              id="email"
+              placeholder="example@mail.com"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input
+              onChange={( e ) => setPassword( e.target.value )}
+              required
+              type="password"
+              name="password"
+              id="password"
+              placeholder="password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white font-semibold py-2 rounded"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
+
+export default Page;
